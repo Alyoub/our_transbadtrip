3123e76a9fa147a70d8af9ae3f334ac920c78d21
Achraf Toukmati <atoukmat@c1r3p1.1337.ma>
2025-02-18 10:40:35 +0100

    yaslam

--- frontend/front_only_container_test.sh
+++ frontend/front_only_container_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Build the Docker image
-docker build -t test .
+docker build -t test_frontend .
 
 # Run the container with port mapping and privileged mode
-docker run --privileged -p 80:80 -it -d test
+docker run --privileged -p 80:80 -it -d test_frontend
 
