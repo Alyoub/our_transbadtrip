e2ae13fe4d48f41340638f5ee1d6125dbf67088e
w0rmr <toukmati2000@gmail.com>
2025-02-24 17:49:43 +0000

    yasalam

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,10 +1,9 @@
-const {google_auth,tow_factor_auth} = require('./user/auth')
-const {register , login, profile ,users} = require('./user/user')
-const {handle_friends} = require('./user/friends')
-const {upload_,change_password,update_, delete_} = require('./user/user_managment')
+const { google_auth, tow_factor_auth } = require('./user/auth');
+const { register, login, profile, users } = require('./user/user');
+const { HandleFriends } = require('./user/friends');
+const { upload_, change_password, update_, delete_ } = require('./user/user_managment');
 const jwt = require('@fastify/jwt');
 const multipart = require('@fastify/multipart');
-
 
 module.exports = async function routes(fastify, options) {
 
@@ -13,10 +12,11 @@ module.exports = async function routes(fastify, options) {
             bad: 'trip'
         };
     });
+    // khasna nhaydo had jwt hit wajda khas n9ado dyalna 
     fastify.register(jwt, {
         secret: ';o2u3ur02435702985ofhladkkhnf;sh@^%$&(&*^#987e093ueor1bnadkljcc'
     });
-    
+
     fastify.decorate('authenticate', async function(request, reply) {
         try {
             await request.jwtVerify();
@@ -24,35 +24,54 @@ module.exports = async function routes(fastify, options) {
             reply.send(err);
         }
     });
-    
-    
-    fastify.register(multipart,{
-        limits:{
-            fileSize : 10 * 1024 * 1024,
+
+    fastify.register(multipart, {
+        limits: {
+            fileSize: 10 * 1024 * 1024,
         },
     });
-    fastify.post('/login/google/',google_auth)
 
-    fastify.post('/friends/:action',{preHandler:[fastify.authenticate]} , async (request,reply)=>{
-        const handler = new handle_friends(request,reply,prisma);
-        return  await handler.handle_request();
+    fastify.post('/login/google/', google_auth);
 
-    })
+    fastify.post('/auth/:action',tow_factor_auth);
 
-    fastify.post('/register',register);
-    
-    fastify.get('/profile',{preHandler:[fastify.authenticate]},profile)
-    
-    fastify.post('/login', async (request,reply )=> { return login(request,reply,fastify) });
+    fastify.post('/friends/:action', { preHandler: [fastify.authenticate] }, async (request, reply) => {
+        const { action } = request.params;
+        const handler = new HandleFriends(request, reply, prisma);
 
-    fastify.get('/users', { preHandler: [fastify.authenticate] },users);
+        switch (action) {
+            case 'add':
+                return handler.addFriend();
+            case 'cancel':
+                return handler.cancelFriend();
+            case 'remove':
+                return handler.removeFriend();
+            case 'list':
+                return handler.listFriends();
+            case 'requests':
+                return handler.friendRequests();
+            case 'accept':
+                return handler.acceptFriend();
+            case 'my_requests':
+                return handler.listRequests();
+            default:
+                return reply.code(400).send({ error: "Invalid action" });
+        }
+    });
 
-    fastify.post('/api/:login/upload', { preHandler: [fastify.authenticate] },upload_ );
+    fastify.post('/register', register);
 
-    fastify.put('/api/:login/change_password', { preHandler: [fastify.authenticate] },change_password);
+    fastify.get('/profile', { preHandler: [fastify.authenticate] }, profile);
+
+    fastify.post('/login', async (request, reply) => { return login(request, reply, fastify); });
+
+    fastify.get('/users', { preHandler: [fastify.authenticate] }, users);
+
+    fastify.post('/api/:login/upload', { preHandler: [fastify.authenticate] }, upload_);
+
+    fastify.put('/api/:login/change_password', { preHandler: [fastify.authenticate] }, change_password);
 
     fastify.put('/user/:login', { preHandler: [fastify.authenticate] }, update_);
 
     fastify.delete('/user/:id', { preHandler: [fastify.authenticate] }, delete_);
-}
-
+};
--- back/src/user/friends.js
+++ back/src/user/friends.js
@@ -1,143 +1,142 @@
-// const { CONSTRAINT } = require('sqlite3');
-
-
-class handle_friends{
-    constructor(request,reply,prisma){
+class HandleFriends {
+    constructor(request, reply, prisma) {
         this.request = request;
-        this.reply =  reply;
+        this.reply = reply;
         this.prisma = prisma;
     }
 
+    async addFriend() {
+        const { friendId } = this.request.body;
+        const { userId } = this.request.user;
 
-    async handle_request(){
-        try{
-        const {action} = this.request.params;
-        switch(action){
-            case 'add':
-                // add friend
-                this.#add_friend();
-            case 'cancel':
-                // cancel friend request
-            case 'remove':
-                // remove friend 
-            case 'list':
-                // list friends
-            case 'requests':
-                // friend requests 
-                this.#friend_requests();
-            case 'accept':
-                // accept friend request 
-                this.#accept_friend();
-            case 'my_requests':
-                // list all the requests that i send 
-                this.#list_requests();
-            default:
-                // error
-                return this.reply.code(69).send({error:"zamel"})
+        if (userId === friendId) {
+            return this.reply.code(400).send({ error: "You cannot add yourself as a friend." });
         }
 
-        
-    } catch (err){
-        console.error('error failed');
-        return this.reply.code(500).send({error:"zeb"});
-    }
+        try {
+            const friendRequest = await this.prisma.friends.create({
+                data: { userId, friendId, accepted: false, date: Math.floor(Date.now() / 1000) },
+            });
+
+            return this.reply.code(201).send({ message: "Friend request sent.", friendRequest });
+        } catch (error) {
+            if (error.code === 'P2002') {
+                return this.reply.code(400).send({ error: "Friend request already exists." });
+            }
+            console.error('Error adding friend:', error);
+            return this.reply.code(500).send({ error: "Internal server error" });
+        }
     }
 
-    async  #friend_requests(){
-        const {userId} = this.request.user;
+    async friendRequests() {
+        const { userId } = this.request.user;
+
         const requests = await this.prisma.friends.findMany({
-            where :{
-                friendId : userId,
-                accepted : false,
-            },
-            include:{
-                user : true,
-            },
-        })
-        if(!requests)
-            return reply.code(777).send({haha:"zebiii "})
-        return reply.code(200).send({yaslam:"tysrat",requests})
-    }
-
-    async #accept_friend(){
-        const {userId} = this.request.user;
-        const {friendId} = this.request.body;
-        const accept = await this.prisma.friends.updateMany({
-            where:{
-                userId: friendId,
-                friendId: userId,
-                accepted: false,
-            },
-            data:{
-                accepted: true,
-                date: Math.floor(Date.now() / 1000),
-            }
-        })
-        if(accept.count === 0)
-            return reply.code(69).send({haha:"haha bad trip"});
-        return reply.code(200).send({haha:"good trip yaslam"});
-    }
-
-    async #add_friend(){
-
-        const {friendId} = this.request.body;
-        const {userId} = this.request.user;
-
-        if(userId === friendId)
-            return reply.code(69).send({hahah:"wch nta hma9 baghi tzid rasek"});
-        is_rquested = await this.prisma.friends.findFirst({
-            where:{
-                userId: userId,
-                friendId: friendId,
-            }
+            where: { friendId: userId, accepted: false },
+            include: { user: true },
         });
-        if(is_rquested)
-            return reply.code(400).send({haha:'deja sifti ya zebi'});
-        const friend_request = await this.prisma.friends.create({
-            data:{
-                userId : userId,
-                friendId : friendId,
-                accepted : false,
-                date: Math.floor(Date.now()/1000),
-            }
-        });
-        return reply.code(200).send({haha:'request tsiftat',friend_request});
-    }
 
-
-    async #list_requests(){
-        const {userId} =  this.request.user;
-        const requests = await this.prisma.friends.findMany({
-            where :{
-                userId : userId,
-                accepted : false, 
-            },
-            include:{
-                user : true,
-            }
-        })
-        console.log("zeb",requests);
-        return reply.code(69).send({
-            haha: "zeb",
+        return this.reply.code(200).send({
+            message: requests.length ? "Incoming friend requests." : "No incoming friend requests.",
             requests,
-        })
-    
+        });
     }
 
-    async  #cancel_friend(){
+    async acceptFriend() {
+        const { userId } = this.request.user;
+        const { friendId } = this.request.body;
 
+        const friendRequest = await this.prisma.friends.findFirst({
+            where: { userId: friendId, friendId: userId, accepted: false },
+        });
 
+        if (!friendRequest) {
+            return this.reply.code(404).send({ error: "Friend request not found." });
+        }
+
+        await this.prisma.friends.update({
+            where: { id: friendRequest.id },
+            data: { accepted: true, date: Math.floor(Date.now() / 1000) },
+        });
+
+        return this.reply.code(200).send({ message: "Friend request accepted." });
     }
-    
-    async  #remove_friend(){
-    
+
+    async listRequests() {
+        const { userId } = this.request.user;
+
+        const requests = await this.prisma.friends.findMany({
+            where: { userId, accepted: false },
+            include: { friend: true },
+        });
+
+        return this.reply.code(200).send({
+            message: requests.length ? "Outgoing friend requests." : "No outgoing friend requests.",
+            requests,
+        });
     }
-    
-    async  #list_friends(){
-    
+
+    async cancelFriend() {
+        const { userId } = this.request.user;
+        const { friendId } = this.request.body;
+
+        const requestExists = await this.prisma.friends.findFirst({
+            where: { userId, friendId, accepted: false },
+        });
+
+        if (!requestExists) {
+            return this.reply.code(404).send({ error: "Friend request not found." });
+        }
+
+        await this.prisma.friends.delete({ where: { id: requestExists.id } });
+
+        return this.reply.code(200).send({ message: "Friend request canceled." });
+    }
+
+    async removeFriend() {
+        const { userId } = this.request.user;
+        const { friendId } = this.request.body;
+
+        const friendship = await this.prisma.friends.findFirst({
+            where: {
+                OR: [
+                    { userId, friendId, accepted: true },
+                    { userId: friendId, friendId: userId, accepted: true },
+                ],
+            },
+        });
+
+        if (!friendship) {
+            return this.reply.code(404).send({ error: "Friendship not found." });
+        }
+
+        await this.prisma.friends.delete({ where: { id: friendship.id } });
+
+        return this.reply.code(200).send({ message: "Friend removed." });
+    }
+
+    async listFriends() {
+        const { userId } = this.request.user;
+
+        const friendships = await this.prisma.friends.findMany({
+            where: {
+                OR: [
+                    { userId, accepted: true },
+                    { friendId: userId, accepted: true },
+                ],
+            },
+            include: { user: true, friend: true },
+        });
+
+        const friends = friendships.map(friendship =>
+            friendship.userId === userId ? friendship.friend : friendship.user
+        );
+
+        return this.reply.code(200).send({
+            message: friends.length ? "Friends list." : "You have no friends yet.",
+            friends,
+        });
     }
 }
 
-
-
-module.exports = {handle_friends}
+module.exports = { HandleFriends };
