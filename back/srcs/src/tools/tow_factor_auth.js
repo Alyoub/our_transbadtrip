cc48f4f7306dc743ea74c2c3bdaf41aeeee07ec8
Achraf Toukmati <atoukmat@c1r3p1.1337.ma>
2025-03-08 20:44:15 +0000

    yaslam

--- back/srcs/src/tools/tow_factor_auth.js
+++ back/srcs/src/tools/tow_factor_auth.js
@@ -33,20 +33,57 @@ class Tow_Facor_Auth{
     static  async generate(params) {
         // generate secret 
 
-        const token = speakeasy.generateSecret();
-        await prisma.update({
-            where:{
-                userId : params.user.userId
-            },
-            data:{
-                tfa_key : token
+        // const token = speakeasy.generateSecret();
+        try{
+                const token = speakeasy.totp({
+                    secret : secret.base32,
+                    encoding: 'base32'
+                });
+                await prisma.update({
+                    where:{
+                        userId : params.user.userId
+                    },
+                    data:{
+                        tfa_key : token
+                    }
+                })
+
+                return params.reply.code(200).send({
+                    goodtrip: 'hadchi khdam',
+                    token : token 
+                });
+            }catch(err){
+                console.log("error bad trip",err);
+                return params.reply.code(500).send({
+                    error: "w9a3 chi error fe hadchi "
+                })
             }
-        })
         // save to databse 
     }
 
     static async Verify(params){
-
+        try{
+            var verify = speakeasy.verify({
+                secret : secret.base32,
+                token: params.request.body.otp,
+                window: 6
+            });
+            if(verify){
+                return params.reply.code(200).send({
+                    yaslam: 'good trip',
+                });
+            } else{
+                return params.reply.code(300).send({
+                    badtrip: "wach nta hacker"
+                });
+            }
+        } catch(err){
+            console.log("bad trip: ",err);
+            return  params.reply.code(500).send({
+                error: "baaad trip",
+                err
+            })
+        }
     }
 }
 
