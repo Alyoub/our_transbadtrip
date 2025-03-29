d19112d9f42fd309cf8389c47bb0c54f2cc12c12
Ayoub Lotfi <alotfi@c1r2p2.1337.ma>
2025-03-29 02:44:58 +0000

    yaslam

--- back/only_backend_test.sh
+++ back/only_backend_test.sh
@@ -2,4 +2,4 @@
 
 docker build -t backend_test .
 
-docker run --env-file .env -p 3000:3000 --privileged -it -d backend_test
+docker run --env-file ~/Desktop/.env -p 3000:3000 --privileged -it -d backend_test
